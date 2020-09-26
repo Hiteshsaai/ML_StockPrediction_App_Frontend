61f9d0fbcc6f087017feb17a8a6ddc17d0dc562a
Hitesh Saai Mananchery Panneerselvam <mppk2009@gmail.com>
2020-09-25 20:36:17 -0600

    Updated App.js with image and heading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Stock Prediction App</h1>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      date: [],
+      stock_values: [],
+      predict_values: []
+    }  
+    
+  }
+
+  render(){
+    return(
+      <div className = "Container">
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500;700;900&display=swap" rel="stylesheet"></link>
+          <img src="https://economictimes.indiatimes.com/thumb/msid-71455217,width-3684,height-2068,resizemode-4,imgsize-550306/stocks-market.jpg?from=mdr" 
+              className= 'image' alt = "stock_image" />
+          <div className = 'heading'><h1> STOCK PREDICTION </h1></div>
+      </div>
+    )
+  }
 }
 
 export default App;
